5e12af6c94e548d0ff1d2f43d82e38488d4bb99c
Augustine <augu.offset@gmail.com>
2025-05-27 18:37:08 +0100

    Delete exercise7.py

--- exercise7.py
+++
@@ -1,9 +0,0 @@
-if 1024 % 4 == 0:
-    print (f'1024 is a multiple of 4')
-else:
-    print (f'1024 is not a multiple of 4')
-
-if 10 % 2 == 0:
-    print (f'10 is a multiple of 2')
-else:
-    print(f'10 is not a multiple of 2')
