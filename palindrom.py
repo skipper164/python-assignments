af248d31d2aec6cd0136b59edaafe8245e76b531
Augustine <augu.offset@gmail.com>
2025-05-27 19:10:04 +0100

    Delete palindrom.py

--- palindrom.py
+++
@@ -1,7 +0,0 @@
-number = int(input('enter a five digit numbeer: ')) 
-thous = number // 1000 
-hund = (number // 100) % 10 
-tens = (number // 10)  % 10 
-unit = number // 10 
- 
-print(unit, tens, hund, thous)
