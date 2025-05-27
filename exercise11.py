765893a353efe10d381158b929f38c718289e2be
Augustine <augu.offset@gmail.com>
2025-05-27 18:38:08 +0100

    Delete exercise11.py

--- exercise11.py
+++
@@ -1,8 +0,0 @@
-number = int(input('enter a five digit numbeer: ')) 
-ten_thous = number // 10000
-thous = (number // 1000) % 10 
-hund = (number // 100) % 10 
-tens = (number // 10)  % 10 
-unit = number % 10 
- 
-print(ten_thous,  thous,  hund,  tens,  unit)
