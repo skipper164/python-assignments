030869e802f04b6ada63fe9c31d63b0eda82e771
Augustine <augu.offset@gmail.com>
2025-05-27 18:44:41 +0100

    Delete exercise8.py

--- exercise8.py
+++
@@ -1,6 +0,0 @@
-print('number    square     cube')
-print('1            1         1  ')
-print('2            4         8  ')
-print('3            9         27 ')
-print('4            16        64 ')
-print('5            25        125')
