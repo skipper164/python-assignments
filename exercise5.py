1db774dda3210a1fa822ef8726af8dd0cfc27187
Augustine <augu.offset@gmail.com>
2025-05-27 18:37:29 +0100

    Delete exercise5.py

--- exercise5.py
+++
@@ -1,8 +0,0 @@
-pie = 3.14159
-radius = 2
-
-diameter = 2 * 2
-circumference = 2 * 3.14159 * 2
-area = (3.14159 * 2) ** 2
-
-print(f'Diameter: {diameter}\n circumference: {circumference}\n Area: {area}')
